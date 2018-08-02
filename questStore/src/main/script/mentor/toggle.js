a484f31944cd0b6209eba767f2379d63eb83ef9d
Seweryn91 <s.talaj@gmail.com>
2018-08-02 12:11:13 +0200

    Fix bug with toggle buttons

--- questStore/src/main/script/mentor/toggle.js
+++ questStore/src/main/script/mentor/toggle.js
@@ -1,66 +1,64 @@
 function toggleToGrade() {
     var x = document.getElementById("gradeSection");
-    var y = document.getElementById("classSection");
-    var z = document.getElementById("toDoSection");
+    var y = document.getElementById("toDoSection");
+    var z = document.getElementById("classSection");
         
-    if ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) {
+    if ( ( (x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) ||
+    ((x.style.display === "none") && (y.style.display ==="block") && (z.style.display === "none")) ||
+    ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "block")) ){
+
         x.style.display = "block";
         y.style.display = "none";
         z.style.display = "none";
 
-    }else if((x.style.display === "block") && (y.style.display == "none") && (z.style.display == "none")){
+    }else if((x.style.display === "block") && (y.style.display === "none") && (z.style.display === "none")){
         x.style.display = "none";
         y.style.display = "none";
         z.style.display = "none";
-
-    }else if((x.style.display === "none") && ((y.style.display == "block") || (z.style.display == "block"))){
-        x.style.display = "block";
-        y.style.display = "none";
-        y.style.display = "none";
     }
+
 }
 
-function toggleClass() {
-    var x = document.getElementById("classSection");
-    var y = document.getElementById("gradeSection");
-    var z = document.getElementById("toDoSection");
-        
-    if ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) {
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "none";
-        
-    }else if((x.style.display === "block") && (y.style.display == "none") && (z.style.display == "none")){
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "none";
-
-    }else if((x.style.display === "none") && ((y.style.display == "block") || (z.style.display == "block"))){
-        x.style.display = "block";
-        y.style.display = "none";
-        y.style.display = "none";
-    }
-}
-
-    function toggleToDo() {
-        var x = document.getElementById("toDoSection");
-        var y = document.getElementById("classSection");
+function toggleToDo() {
+        var x = document.getElementById("gradeSection");
+        var y = document.getElementById("toDoSection");
         var z = document.getElementById("classSection");
             
-        if ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) {
-            x.style.display = "block";
-            y.style.display = "none";
+        if ( ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) || 
+        ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "block")) ||
+        ((x.style.display === "block") && (y.style.display === "none") && (z.style.display === "none")) ){
+            x.style.display = "none";
+            y.style.display = "block";
             z.style.display = "none";
-            
-        }else if((x.style.display === "block") && (y.style.display == "none") && (z.style.display == "none")){
+    
+        }else if((x.style.display === "none") && (y.style.display ==="block") && (z.style.display === "none")){
+           
             x.style.display = "none";
             y.style.display = "none";
             z.style.display = "none";
-
-        }else if((x.style.display === "none") && ((y.style.display == "block") || (z.style.display == "block"))){
-            x.style.display = "block";
-            y.style.display = "none";
-            y.style.display = "none";
         }
+}
+
+    function toggleClass() {
+        var x = document.getElementById("gradeSection");
+        var y = document.getElementById("toDoSection");
+        var z = document.getElementById("classSection");
+            
+        if ( ((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "none")) ||
+            ((x.style.display === "block") && (y.style.display ==="none") && (z.style.display === "none")) ||
+            ((x.style.display === "none") && (y.style.display === "block") && (z.style.display === "none")) ){
+
+            x.style.display = "none";
+            y.style.display = "none";
+            z.style.display = "block";
+    
+    
+        }else if((x.style.display === "none") && (y.style.display === "none") && (z.style.display === "block")){
+            
+            x.style.display = "none";
+            y.style.display = "none";
+            z.style.display = "none";
+        }
+
     }
 
