d2ae9a6c8c0e0624c28de84b6eb9a003a28dfc2b
polon59 <przemyslaw.polczak@gmail.com>
2018-08-30 00:30:41 +0200

    Create class 'Group.java' with fields defined in UML diagram

---
+++ questStore/src/main/java/com/codecool/app/model/Group.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Group {
+    // private Student admin;
+    // private HashMap<Student,Integer> members;
+    // private String name;
+    // private int wallet;
+
+    // public Group(Student admin, HashMap<Student,Integer> members, String name){
+    //     this.admin = admin;
+    //     this.members = members;
+    //     this.name = name;
+    // }
+
+}
