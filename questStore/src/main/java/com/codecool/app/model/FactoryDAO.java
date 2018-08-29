2f37f6cd38182253c266655eefe3503c384e1d98
polon59 <przemyslaw.polczak@gmail.com>
2018-08-29 21:55:12 +0200

    Add methods to create ClassroomDAO instance

--- questStore/src/main/java/com/codecool/app/model/FactoryDAO.java
+++ questStore/src/main/java/com/codecool/app/model/FactoryDAO.java
@@ -13,6 +13,7 @@ public class FactoryDAO {
     UserDAO userDAO;
     ManagerDAO managerDAO;
     MentorDAO mentorDAO;
+    ClassroomDAO classroomDAO;
     
 
 
@@ -24,6 +25,7 @@ public class FactoryDAO {
         userDAO = new UserDAO(connection, commonDAO);
         managerDAO = new ManagerDAO(connection, commonDAO);
         mentorDAO = new MentorDAO(connection, commonDAO);
+        classroomDAO = new ClassroomDAO(connection, commonDAO);
     }
 
 
@@ -44,5 +46,10 @@ public class FactoryDAO {
 
     public MentorDAO getMentorDAO() {
         return this.mentorDAO;
+    }
+
+
+    public ClassroomDAO getClassroomDAO() {
+        return this.classroomDAO;
     }
 }
