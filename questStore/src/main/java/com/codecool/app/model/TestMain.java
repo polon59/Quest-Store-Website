df28c723b8720e119a540e52785049034e90eda6
polon59 <przemyslaw.polczak@gmail.com>
2018-08-29 11:37:47 +0200

    Add DAO testing methods in class 'TestMain.java'

--- questStore/src/main/java/com/codecool/app/model/TestMain.java
+++ questStore/src/main/java/com/codecool/app/model/TestMain.java
@@ -11,10 +11,42 @@ public class TestMain {
         Connection connection = dbConnection.getConnection();
         UserDAO userDAO = new UserDAO(connection);
 
-        userDAO.setEmail("NOWY IMEJL", 3);
-        userDAO.setPhone("NOWY FOŁN", 2);
+        // test email set
+        userDAO.setEmail("new email", 3);
+
+        // test phone set
+        userDAO.setPhone("new phone", 2);
+
+        //test finding user by email
+        System.out.println("test finding user by email:");
         System.out.println(userDAO.findUserByEmail("ff").toString());
+
+
+        //test finding user by id
+        System.out.println("test finding user by id:");
+        User foundUser =  userDAO.findUserById(3);
+        System.out.println(foundUser.toString()); 
         
+
+        //test finding user's name
+        System.out.println("test finding user's name:");
+        String name = userDAO.getName(3);
+        System.out.println(name);
+
+        // test finding user's surname
+        System.out.println("test finding user's surname");
+        String surname = userDAO.getSurname(3);
+        System.out.println(surname);
+
+        // test finding user's email
+        System.out.println("test finding user's email");
+        String email = userDAO.getEmail(3);
+        System.out.println(email);
+
+        // test finding user's phone
+        System.out.println("test finding user's phone");
+        String phone = userDAO.getPhone(3);
+        System.out.println(phone);
     }
 
 }
